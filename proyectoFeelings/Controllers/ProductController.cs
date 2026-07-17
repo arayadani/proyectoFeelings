58529111e09ee248e3511c89b8fbf44548750750
arayadani <arayadani33@gmail.com>
2026-07-16 23:50:23 -0600

    se arreglo el unknown de las tiendas y cantidad quedo broken edit product

--- proyectoFeelings/Controllers/ProductController.cs
+++ proyectoFeelings/Controllers/ProductController.cs
@@ -67,8 +67,11 @@ namespace proyectoFeelings.Controllers
             };
             _context.StoreProduct.Add(StoreProduct);
             await _context.SaveChangesAsync(); // Save the StoreProduct entity to the database
-            return RedirectToAction("Index", "Home");
+            TempData["SuccessMessage"] = "Producto creado correctamente";
+            return RedirectToAction(nameof(ProductList));
         }
+
+
         //ProductList
         [HttpGet]
         public IActionResult ProductList()
@@ -77,18 +80,58 @@ namespace proyectoFeelings.Controllers
             var Products = _context.Product
         .Select(u => new ProductViewModel
         {
+            ProductID = u.ProductID,
             Code = u.Code,
             Description = u.Description,
             Price = u.Price,
             Provider = u.Provider,
             Status = u.Status,
+            StoreID = u.StoreProduct.FirstOrDefault().StoreID, // Assuming you want the StoreID from the first StoreProduct
             Category = u.Category,
+            Quantity = u.StoreProduct.FirstOrDefault().Quantity,
 
         })
         .ToList();
 
             return View(Products);
 
+        }
+        //edit product
+
+        [HttpGet]
+        public async Task<IActionResult> EditProduct(String productId, String storeId)
+
+        {
+            var ProductId = Convert.ToInt32(productId);
+            var StoreId = Convert.ToInt32(storeId);
+
+
+            if (ProductId == null || StoreId == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Product.FindAsync(ProductId);
+            var storeProduct = await _context.StoreProduct.FindAsync(ProductId, StoreId);
+
+            if (storeProduct == null)
+            {
+                return NotFound();
+
+            }
+            var model = new ProductViewModel
+            {
+                ProductID = product.ProductID,
+                Code = product.Code,
+                Description = product.Description,
+                Price = product.Price,
+                Provider = product.Provider,
+                Status = product.Status,
+                StoreID = storeProduct.StoreID, // Assuming you want the StoreID from the first StoreProduct
+                Category = product.Category,
+                Quantity = storeProduct.Quantity,
+            };
+
+            return View(model);
         }
 
     }
